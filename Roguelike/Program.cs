bea09f3765ee795550e9106ee6b37c326f291ad5
Marco Domingos <supersthotaku@gmail.com>
2020-06-01 20:57:12 +0100

    Add namespaces and class on Board.cs, Game.cs, add UI.cs class

--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -7,7 +7,6 @@ namespace Roguelike
         static void Main(string[] args)
         {
             
-            
         }
     }
 }
---
+++ Roguelike/UI.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Roguelike
+{
+    public class UI
+    {
+        // The single string that manages all inputs on the game.
+        public string Input { get; private set; } = "";
+        // A splitted array of strings used to read commands.
+        public string[] SplitInput { get; private set;} 
+
+        private void MainMenu()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("|        Roguelike        |");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("| n - New Game            |");
+            Console.WriteLine("| h - High Scores         |");
+            Console.WriteLine("| i - Instructions        |");
+            Console.WriteLine("| c - Credits             |");
+            Console.WriteLine("| q - Quit                |");
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
