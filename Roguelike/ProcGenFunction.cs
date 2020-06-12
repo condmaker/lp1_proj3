e62d51af36954f95c3c564df3a322bd46229cb5e
Marco Domingos <supersthotaku@gmail.com>
2020-06-12 19:15:33 +0100

    Update README.md

--- Roguelike/ProcGenFunction.cs
+++ Roguelike/ProcGenFunction.cs
@@ -10,33 +10,6 @@ namespace Roguelike
     /// </summary>
     public static class ProcGenFunctions
     {
-
-        /// <summary>
-        /// Linear function
-        /// </summary>
-        /// <param name="x">Input variable x</param>
-        /// <param name="m">Slope</param>
-        /// <param name="b">Intercept value at yy</param>
-        /// <returns>The y output variable</returns>
-        public static double Linear(double x, double m, double b)
-        {
-            return m * x + b;
-        }
-
-        /// <summary>
-        /// Piecewise linear function
-        /// </summary>
-        /// <param name="x">Input variable x</param>
-        /// <param name="m">Slope</param>
-        /// <param name="b">Intercept value at yy</param>
-        /// <param name="l">Value of x which separates the sloped and constant
-        /// parts of the function.</param>
-        /// <returns>The y output variable</returns>
-        public static double PiecewiseLinear(double x, double m, double b, double l)
-        {
-            return x < l ? m * x + b : m * l + b;
-        }
-
         /// <summary>
         /// Logistic function
         /// </summary>
@@ -48,18 +21,6 @@ namespace Roguelike
         public static double Logistic(double x, double L, double x0, double k)
         {
             return L / (1 + Math.Exp(-k * (x - x0)));
-        }
-
-        /// <summary>
-        /// Logarithmic function
-        /// </summary>
-        /// <param name="x">Input variable x</param>
-        /// <param name="a">Scale modifier</param>
-        /// <param name="d">Base modifier</param>
-        /// <returns>The y output variable</returns>
-        public static double Log(double x, double a, double d)
-        {
-            return a * Math.Log(d * x);
         }
     }
 }
